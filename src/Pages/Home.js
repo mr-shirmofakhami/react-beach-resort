2bc1de3d5e80856e6d629afadc22ce473c6cbf45
mr-shirmofakhami <mr.shirmofakhami@gmail.com>
2020-06-15 13:15:43 +0430

    first code

---
+++ src/Pages/Home.js
@@ -0,0 +1,21 @@
+import  React from 'react';
+import Hero from "../component/Hero";
+import Banner from "../component/Banner";
+import {Link} from 'react-router-dom';
+import Services from "../component/Services";
+import FeaturedRoom from "../component/FeaturedRoom";
+export default function Home() {
+    return (
+        <>
+        <Hero>
+            <Banner title="luxurious rooms" subtitle="delux rooms starting at $299">
+                <Link to='/rooms' className="btn-primary">
+                    our rooms
+                </Link>
+            </Banner>
+        </Hero>
+            <Services />
+            <FeaturedRoom />
+        </>
+    );
+};
